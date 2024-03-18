69bf86e31275aa7d89937efd289459211752cd34
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-03-18 06:32:27 +0400

    feat: add features to deploy the project

--- api/index.js
+++ api/index.js
@@ -2,14 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import countryRouter from "./routes/country.route.js";
-import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 const app = express(); // Create an instance of Express
 const PORT = process.env.PORT || 3001; // Define the port number from environment variables or default to 3001
 const MONGO_URL = process.env.MONGO_URL; // Get MongoDB URL from environment variables
-
-app.use(cookieParser());
 
 // Connect to MongoDB
 mongoose
@@ -21,9 +19,17 @@ mongoose
     console.log(error);
   });
 
+const __dirname = path.resolve();
+
 // Start the server
 app.listen(PORT, () => {
   console.log("Server is running on port 3001");
 });
 
 app.use("/api", countryRouter); // Mount the countryRouter under the /api endpoint
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
