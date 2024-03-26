06a55b3f7d190bc73320f56e962debb185e22799
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-03-26 07:22:54 +0400

    update gitignore, remove .env and add vercel.json files

--- api/index.js
+++ api/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import countryRouter from "./routes/country.route.js";
-import path from "path";
+import cors from "cors";
 dotenv.config();
 
 const app = express(); // Create an instance of Express
 const PORT = process.env.PORT || 3001; // Define the port number from environment variables or default to 3001
 const MONGO_URL = process.env.MONGO_URL; // Get MongoDB URL from environment variables
+
+app.use(cors());
+
+app.use(express.json());
 
 // Connect to MongoDB
 mongoose
@@ -19,17 +23,9 @@ mongoose
     console.log(error);
   });
 
-const __dirname = path.resolve();
-
 // Start the server
 app.listen(PORT, () => {
   console.log("Server is running on port 3001");
 });
 
 app.use("/api", countryRouter); // Mount the countryRouter under the /api endpoint
-
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
 });
